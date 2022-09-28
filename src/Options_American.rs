a584eac02a68189eae5f33771a21820e4aaf7234
amando <avhzanderigo@gmail.com>
2022-09-28 12:02:29 +0200

    Refactored american put

--- src/Options_American.rs
+++ src/Options_American.rs
@@ -14,14 +14,14 @@ pub fn american_put(S0: f64, K: f64, T: f64, r: f64, sigma: f64, q: f64, n: usiz
     //   '  n... height of the binomial tree
     let deltaT: f64 = T / (n as f64);
     let up: f64 = (sigma * deltaT.sqrt()).exp();
-    let p0: f64 = (up * (-q * deltaT).exp() - (-r * deltaT)).exp() / (up * up - 1.);
+    let p0: f64 = (up * (-q * deltaT).exp() - (-r * deltaT).exp()) / (up * up - 1.);
     let p1: f64 = (-r * deltaT).exp() - p0;
 
     // let mut p: Vec<f64> = Vec::with_capacity(n);
-    let mut p: Vec<f64> = vec![0.0; n];
+    let mut p: Vec<f64> = vec![0.0; n + 1];
 
     //   ' initial values at time T
-    for i in 0..n {
+    for i in 0..=n {
         p[i] = K - S0 * up.powf(2. * i as f64 - n as f64);
         if p[i] < 0.0 {
             p[i] = 0.0;
@@ -35,7 +35,7 @@ pub fn american_put(S0: f64, K: f64, T: f64, r: f64, sigma: f64, q: f64, n: usiz
             //   ' exercise value
             let exercise: f64 = K - S0 * up.powf(2. * i as f64 - j as f64);
             if p[i] < exercise {
-                p[i] = exercise
+                p[i] = exercise;
             };
         }
     }
@@ -52,8 +52,8 @@ mod tests {
 
     #[test]
     fn american_put_test() {
-        let price = american_put(100., 110., 180. / 365., 0.05, 0.2, 0.0, 100);
+        let price = american_put(100., 110., 180. / 365., 0.05, 0.2, 0.0, 5000);
         println!("{}", price);
-        assert!(price - 10.9546 < 0.0001);
+        assert!(price - 10.9546 < 0.01);
     }
 }
