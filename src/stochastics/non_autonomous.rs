97277ef5f15984c1465dcf4444482f6fe0f93992
Amando Z <avhzanderigo@gmail.com>
2022-11-19 16:07:58 +0100

    started stochastic processes

--- src/stochastics/non_autonomous.rs
+++ src/stochastics/non_autonomous.rs
@@ -2,3 +2,56 @@
 //! Non-autonomous refers to differential equations that are an explicit
 //! function of time `t`.
 //!
+
+//! Module containing various stochastic processes.
+
+/// Base trait for a stochastic process.
+
+/// Non-autonomous stochastic process.
+///
+/// A non-autonomous stochastic process is a stochastic process in which the drift and
+/// diffusion are functions that depend on time.
+pub trait NonautonomousStochasticProcess: StochasticProcess {
+    fn drift(&self, t: f32, x: f32) -> f32;
+
+    fn diffusion(&self, t: f32, x: f32) -> f32;
+
+    fn run_euler_maruyama(&self, x_0: f32, t_0: f32, t_n: f32, n: usize) -> crate::SimulatedPath {
+        use rand::prelude::Distribution;
+
+        let dt: f32 = (t_n - t_0) / (n as f32);
+
+        // Set up rng.
+        let mut rng = rand::thread_rng();
+        let increments: Vec<f32> = match statrs::distribution::Normal::new(0.0, 1.0) {
+            Ok(dist) => dist,
+            Err(_) => panic!("Bad parameters."),
+        }
+        .sample_iter(&mut rng)
+        .take(n)
+        .map(|x| (x as f32) * dt.sqrt())
+        .collect();
+
+        // Construct empty path.
+        let mut path = crate::SimulatedPath::zeros_generic(
+            nalgebra::Dim::from_usize(n + 1),
+            nalgebra::Dim::from_usize(2),
+        );
+
+        // Fill in t.
+        path[(0, 0)] = t_0;
+        path[(n, 0)] = t_n;
+        for t in 1..(n + 1) {
+            path[(t, 0)] = t_0 + dt * (t as f32);
+        }
+
+        // Fill in x.
+        path[(0, 1)] = x_0;
+        for t in 0..n {
+            path[(t + 1, 1)] = path[(t, 1)]
+                + self.drift(path[(t, 0)], path[(t, 1)]) * (path[(t + 1, 0)] - path[(t, 0)])
+                + self.diffusion(path[(t, 0)], path[(t, 1)]) * increments[t];
+        }
+        path
+    }
+}
--- src/stochastics/process.rs
+++ src/stochastics/process.rs
@@ -1,2 +1,8 @@
 //! Module that contains the struct `Process`.
 //! This is the return type of all the stochastic processes.
+
+pub struct Process {
+    trajectory: Vec<f64>,
+}
+
+pub trait StochasticProcess {}
