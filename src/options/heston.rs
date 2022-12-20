f4b2e9cea47eb3cac857a9a8d00c87a016abb612
Amando Z <avhzanderigo@gmail.com>
2022-12-20 01:22:41 +0100

    Tests for Heston model option pricer.

--- src/options/heston.rs
+++ src/options/heston.rs
@@ -4,27 +4,28 @@ use crate::math::*;
 use num_complex::Complex;
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// STRUCTS
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// IMPLEMENTATIONS
+// FUNCTIONS
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /// Heston model for option pricing.
 pub fn heston(
-    S0: f64,
-    V0: f64,
-    K: f64,
-    tau: f64,
-    r: f64,
-    q: f64,
-    rho: f64,
-    sigma: f64,
-    kappa: f64,
-    theta: f64,
+    S0: f64,    // Initial asset value.
+    V0: f64,    // Initial variance value.
+    K: f64,     // Strike price.
+    tau: f64,   // Time to expiry.
+    r: f64,     // Risk-free rate.
+    q: f64,     // Dividend yield.
+    rho: f64,   // Correlation between the two Brownian motions.
+    sigma: f64, // Volatility-of-volatility.
+    kappa: f64, // Mean reversion rate in the variance process' drift term.
+    theta: f64, // Long run mean of the variance process.
 ) -> (f64, f64) {
-    let lambda = 0.0; // Market price of volatility risk (set to 0 for simplicity).
+    // Market price of volatility risk (set to 0 for simplicity).
+    // Should probably include, though, since for equity options it has been shown
+    // to be non-zero (Lamoureux & Lastrapes, 1993).
+    let lambda = 0.0;
+
+    // i = sqrt(-1). Used frequently, so assign here.
     let i: Complex<f64> = Complex::i();
 
     let u = |j: u8| -> f64 {
@@ -44,8 +45,9 @@ pub fn heston(
     };
 
     let d = |j: u8, phi: f64| -> Complex<f64> {
-        ((rho * sigma * i * phi).powi(2) - sigma.powi(2) * (2.0 * u(j) * i * phi - phi.powi(2)))
-            .sqrt()
+        ((rho * sigma * i * phi - b(j)).powi(2)
+            - sigma.powi(2) * (2.0 * u(j) * i * phi - phi.powi(2)))
+        .sqrt()
     };
 
     let g = |j: u8, phi: f64| -> Complex<f64> {
@@ -59,7 +61,7 @@ pub fn heston(
 
         (r - q) * i * phi * tau
             + (kappa * theta / sigma.powi(2))
-                * ((b(j) - rho * sigma * i * phi) * tau
+                * ((b(j) - rho * sigma * i * phi + d(j, phi)) * tau
                     - 2.0 * ((1.0 - g(j, phi) * (d(j, phi) * tau).exp()) / (1.0 - g(j, phi))).ln())
     };
 
@@ -70,12 +72,14 @@ pub fn heston(
             / (sigma.powi(2) * (1.0 - g(j, phi) * (d(j, phi) * tau).exp()))
     };
 
+    // The Heston characteristic functions.
     let f = |j: u8, phi: f64| -> Complex<f64> {
         assert!(j == 1 || j == 2);
 
         (C(j, phi) + D(j, phi) * V0 + i * phi * S0.ln()).exp()
     };
 
+    // These functions return the integrand for P1 and P2.
     let Re1 = |phi: f64| -> f64 {
         let j = 1;
 
@@ -87,9 +91,12 @@ pub fn heston(
         (f(j, phi) * (-i * phi * K.ln()).exp() / (i * phi)).re
     };
 
+    // Integration bounds given in Fabrice D. Rouah's book (see tests).
+    // The integral decays rapidly so 50 is probably enough.
     let P1 = 0.5 + std::f64::consts::FRAC_1_PI * integrate(Re1, 0.00001, 50.0);
     let P2 = 0.5 + std::f64::consts::FRAC_1_PI * integrate(Re2, 0.00001, 50.0);
 
+    // Price call, then use put-call-parity for the put.
     let call = S0 * (-q * tau).exp() * P1 - K * (-r * tau).exp() * P2;
     let put = call + K * (-r * tau).exp() - S0 * (-q * tau).exp();
 
@@ -101,7 +108,8 @@ pub fn heston(
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /*
-TEST VALUES:
+TEST VALUES
+From: "The Heston Model and Its Extensions in MATLAB and C#"" by Fabrice D. Rouah.
 
 ""
 For example, the price a 6-month European put with strike K = 100 on a
@@ -118,17 +126,24 @@ the put price decreases, to 5.3790, and the call price increases, to 6.8678.
 
 #[cfg(test)]
 mod tests {
+    use crate::assert_approx_equal;
+
     use super::*;
 
     #[test]
-    fn test_heston_call() {
-        println!(
-            "Call: \t{}\nPut: \t{}",
-            // heston(100.0, 0.05, 100.0, 0.5, 0.03, 0.02, -0.5, 0.0, 5.0, 0.05)
-            heston(100.0, 0.05, 100.0, 0.5, 0.03, 0.02, -0.8, 0.5, 5.0, 0.05).0,
-            heston(100.0, 0.05, 100.0, 0.5, 0.03, 0.02, -0.8, 0.5, 5.0, 0.05).1
-        );
+    fn test_heston_options() {
+        // WITH DIVIDEND YIELD.
+        let heston1 = heston(100.0, 0.05, 100.0, 0.5, 0.03, 0.02, -0.8, 0.5, 5.0, 0.05);
+        // Call price.
+        assert_approx_equal!(heston1.0, 6.2528, 1e-4);
+        // Put price.
+        assert_approx_equal!(heston1.1, 5.7590, 1e-4);
 
-        assert!(1 == 0);
+        // WITHOUT DIVIDEND YIELD.
+        let heston2 = heston(100.0, 0.05, 100.0, 0.5, 0.03, 0.0, -0.8, 0.5, 5.0, 0.05);
+        // Call price.
+        assert_approx_equal!(heston2.0, 6.8678, 1e-4);
+        // Put price.
+        assert_approx_equal!(heston2.1, 5.3790, 1e-4);
     }
 }
