cb8523ef9f647666b0dbfdc9f0256d93a91828e0
aatmunbaxi <abaxi@tamu.edu>
2023-06-05 17:57:25 -0500

    Theta time-varying, sigma constant

--- src/stochastics/hull_white.rs
+++ src/stochastics/hull_white.rs
@@ -8,32 +8,32 @@ use crate::stochastics::*;
 
 /// Struct containing the Hull-White process parameters.
 pub struct HullWhite {
-    /// Long run mean ($\mu$)
+    /// Long run mean ($\alpha)
     pub alpha: f64,
-    /// Non-negative diffusion, or instantaneous time-varying volatility ($\sigma$).
-    pub sigma_t: fn(f64) -> f64,
-    /// Mean reversion function ($\theta_t$)
-    pub theta: f64,
+    /// Non-negative diffusion, or instantaneous volatility ($\sigma$).
+    pub sigma: f64,
+    /// Mean reversion function (non-negative) ($\theta(t)$)
+    pub theta_t: fn(f64) -> f64,
 }
 
 impl HullWhite {
     /// Create a new Hull-White process.
-    pub fn new(alpha: f64, sigma_t: fn(f64) -> f64, theta: f64) -> Self {
+    pub fn new(alpha: f64, sigma: f64, theta_t: fn(f64) -> f64) -> Self {
         Self {
             alpha,
-            sigma_t,
-            theta,
+            sigma,
+            theta_t,
         }
     }
 }
 
 impl StochasticProcess for HullWhite {
-    fn drift(&self, x: f64, _t: f64) -> f64 {
-        self.theta - (self.alpha * x)
+    fn drift(&self, x: f64, t: f64) -> f64 {
+        (self.theta_t)(t) - (self.alpha * x)
     }
 
-    fn diffusion(&self, _x: f64, t: f64) -> f64 {
-        (self.sigma_t)(t)
+    fn diffusion(&self, _x: f64, _t: f64) -> f64 {
+        self.sigma
     }
 
     fn jump(&self, _x: f64, _t: f64) -> f64 {
@@ -50,16 +50,16 @@ mod tests_hull_white {
     use super::*;
     use crate::{assert_approx_equal, utilities::*};
 
-    fn sigma_t(_t: f64) -> f64 {
-        2.0
+    fn theta_t(_t: f64) -> f64 {
+        0.5
     }
     #[test]
     fn test_hull_white() -> Result<(), Box<dyn std::error::Error>> {
         let alpha = 2.0;
-        let theta = 0.3;
-        let sig = sigma_t(1.0);
+        let theta = theta_t(0.0);
+        let sig = 2.0;
 
-        let hw = HullWhite::new(alpha, sigma_t, theta);
+        let hw = HullWhite::new(alpha, sig, theta_t);
 
         let output = hw.euler_maruyama(10.0, 0.0, 1.0, 150, 1000, false);
 
@@ -71,7 +71,7 @@ mod tests_hull_white {
             .collect();
 
         let E_XT = mean(&X_T, MeanType::Arithmetic);
-        // E[X_T] = X_0*exp(-alpha T) X_0 + (theta/alpha)(1- exp(-alpha * T))
+        // E[X_T] = X_0*exp(-alpha T) X_0 + (theta_t/alpha)(1- exp(-alpha * T))
         assert_approx_equal!(
             E_XT,
             (-alpha * 1.0 as f64).exp() * 10.0
