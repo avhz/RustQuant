2b35c82564e761f5b7e6034c9c3bc513d1893b10
Amando Z <avhzanderigo@gmail.com>
2023-07-02 20:32:26 +0200

    Interactive

--- src/interactive/main.rs
+++ src/interactive/main.rs
@@ -13,12 +13,12 @@
 // documented with a SAFETY comment.
 #![forbid(clippy::undocumented_unsafe_blocks)]
 
-// use crossterm::event;
-// use crossterm::terminal::enable_raw_mode;
+/// The banner for the interactive binary.
+pub mod banner;
+
+use banner::BANNER;
 use serde_json::Value;
-// use std::thread;
-// use std::time::{Duration, Instant};
-use std::{fs::File, sync::mpsc};
+use std::fs::File;
 
 const DB: &str = "./src/interactive/db.json";
 
@@ -28,53 +28,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Hello, {}!", json.get("users").unwrap());
 
-    // enable_raw_mode().expect("can run in raw mode");
-
-    // let (tx, rx) = mpsc::channel();
-    // let tick_rate = Duration::from_millis(200);
-    // thread::spawn(move || {
-    //     let mut last_tick = Instant::now();
-    //     loop {
-    //         let timeout = tick_rate
-    //             .checked_sub(last_tick.elapsed())
-    //             .unwrap_or_else(|| Duration::from_secs(0));
-
-    //         if event::poll(timeout).expect("poll works") {
-    //             if let CEvent::Key(key) = event::read().expect("can read events") {
-    //                 tx.send(Event::Input(key)).expect("can send events");
-    //             }
-    //         }
-
-    //         if last_tick.elapsed() >= tick_rate {
-    //             if let Ok(_) = tx.send(Event::Tick) {
-    //                 last_tick = Instant::now();
-    //             }
-    //         }
-    //     }
-    // });
+    println!("{}", BANNER);
 
     Ok(())
 }
-
-// enum Event<I> {
-//     Input(I),
-//     Tick,
-// }
-
-// enum MenuItem {
-//     Home,
-//     Pricing,
-//     About,
-//     Quit,
-// }
-
-// impl From<MenuItem> for usize {
-//     fn from(input: MenuItem) -> usize {
-//         match input {
-//             MenuItem::Home => 0,
-//             MenuItem::Pricing => 1,
-//             MenuItem::About => 2,
-//             MenuItem::Quit => 3,
-//         }
-//     }
-// }
