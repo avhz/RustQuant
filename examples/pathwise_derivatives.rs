2b32e37d1f2842b2c59fd6d23938a2b746b6f71c
amando <avhzanderigo@gmail.com>
2023-07-19 18:18:45 +0200

    Update example.

--- examples/pathwise_derivatives.rs
+++ examples/pathwise_derivatives.rs
@@ -10,70 +10,80 @@ use rand_distr::{Distribution, Normal};
 use RustQuant::{autodiff::*, instruments::options::TypeFlag};
 
 fn main() {
-    let n_sims = 200;
-    let n_assets = 200;
+    let s = 5.;
+    let t = 1.;
+    let k = 5.;
+    let r = 0.1;
+    let v = 0.5;
+    let n_sims = 500;
+    let n_assets = 500;
 
-    // Just increase the number of simulations.
+    println!("millis, spot, time, price, delta, vega, theta, rho");
+
+    // Vary over time and underlying.
+    // for t in (1..=10).map(|x| x as f64 / 10.) {
+    //     for s in (1..=10).step_by(1) {
+    //         test_basket_option(s as f64, k, t as f64, r, v, n_sims, n_assets);
+    //     }
+    // }
+
+    // // Just increase the number of simulations.
     for i in (10..=n_sims).step_by(10) {
-        test_basket_option(i, 100);
+        test_basket_option(s, k, t, r, v, i, 100);
     }
 
-    // Just increase the number of assets.
+    // // Just increase the number of assets.
     for j in (10..=n_assets).step_by(10) {
-        test_basket_option(100, j);
+        test_basket_option(s, k, t, r, v, 100, j);
     }
+
+    // println!("millis, n_sims, n_assets, price, delta, vega, theta, rho");
 
     // Increase both the number of simulations and the number of assets.
     // for i in (10..=n_sims).step_by(10) {
     //     for j in (10..=n_assets).step_by(10) {
-    //         test_basket_option(i, j);
+    //         test_basket_option(s, k, t, r, v, i, j);
     //     }
     // }
 }
 
-fn test_basket_option(n_sims: usize, n_assets: usize) {
-    let mut basket_option = BasketOption {
-        price: 0.,
-        delta: 0.,
-        vega: 0.,
-        theta: 0.,
-        rho: 0.,
-    };
+fn test_basket_option(s: f64, k: f64, t: f64, r: f64, v: f64, n_sims: usize, n_assets: usize) {
+    let mut basket_option = BasketOption::new();
 
     // Allocate a new graph.
     let g = Graph::new();
 
     // Allocate variables.
-    let strike = 10.; // We don't differentiate with respect to the strike.
-    let spot = g.var(10.);
-    let time = g.var(1.);
-    let drift = g.var(0.1);
-    let diffusion = g.var(0.5);
+    let strike = k; // We don't differentiate with respect to the strike.
+    let spot = g.var(s);
+    let time = g.var(t);
+    let drift = g.var(r);
+    let diffusion = g.var(v);
 
     // Allocate a vector for the basket option.
     let mut basket: Vec<Variable> = Vec::with_capacity(2 * n_assets);
 
     let start = std::time::Instant::now();
     for _ in 0..n_sims {
+        basket.clear();
+
         // Generate paths of the underlying assets.
         for _ in 0..n_assets {
-            let path = Pathwise::gbm(spot, time, drift, diffusion);
-            basket.push(path);
+            basket.push(Pathwise::gbm(spot, time, drift, diffusion));
         }
 
         // Compute the basket (equally) weighted sum.
-        let s = basket.iter().copied().sum::<Variable>() / 100.;
-
-        // let s = Pathwise::gbm(spot, time, drift, diffusion);
+        let s = basket.iter().copied().sum::<Variable>() / n_assets as f64;
 
         // Compute the payoff and discount it.
         let discount = Pathwise::df(drift, time);
-        let payoff = discount * Pathwise::payoff(s, strike, TypeFlag::Call);
+        let payoff = Pathwise::payoff(s, strike, TypeFlag::Call);
+        let discounted_payoff = discount * payoff;
 
-        basket_option.price += payoff.value();
+        basket_option.price += discounted_payoff.value();
 
         // Accumulate the gradient.
-        let gradient = payoff.accumulate();
+        let gradient = discounted_payoff.accumulate();
 
         // Differentiate with respect to the parameters.
         basket_option.delta += gradient.wrt(&spot);
@@ -83,15 +93,31 @@ fn test_basket_option(n_sims: usize, n_assets: usize) {
     }
     let end = start.elapsed();
 
-    // println!("Price \t= {}", basket_option.price / n_sims as f64);
-    // println!("Delta \t= {}", basket_option.delta / n_sims as f64);
-    // println!("Vega \t= {}", basket_option.vega / n_sims as f64);
-    // println!("Theta \t= {}", basket_option.theta / n_sims as f64);
-    // println!("Rho \t= {}", basket_option.rho / n_sims as f64);
-
-    // println!("Wall time: i={}, j={}, time = {:?}", i, j, end.as_millis());
-    // println!("{n_sims},{n_assets},{:?}", end.as_millis());
     print!("{:?},", end.as_millis());
+
+    // println!(
+    //     "{:.4},{:.1},{:.1},{:.4},{:.4},{:.4},{:.4},{:.4}",
+    //     end.as_millis(),
+    //     s,
+    //     t,
+    //     basket_option.price / n_sims as f64,
+    //     basket_option.delta / n_sims as f64,
+    //     basket_option.vega / n_sims as f64,
+    //     basket_option.theta / n_sims as f64,
+    //     basket_option.rho / n_sims as f64
+    // );
+
+    // println!(
+    //     "{:.4},{:.4},{:.4},{:.4},{:.4},{:.4},{:.4},{:.4}",
+    //     end.as_millis(),
+    //     n_sims,
+    //     n_assets,
+    //     basket_option.price / n_sims as f64,
+    //     basket_option.delta / n_sims as f64,
+    //     basket_option.vega / n_sims as f64,
+    //     basket_option.theta / n_sims as f64,
+    //     basket_option.rho / n_sims as f64
+    // );
 }
 
 struct BasketOption {
@@ -100,6 +126,18 @@ struct BasketOption {
     vega: f64,
     theta: f64,
     rho: f64,
+}
+
+impl BasketOption {
+    fn new() -> Self {
+        Self {
+            price: 0.,
+            delta: 0.,
+            vega: 0.,
+            theta: 0.,
+            rho: 0.,
+        }
+    }
 }
 
 struct Pathwise {}
