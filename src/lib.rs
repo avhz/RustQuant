80585c5d6cc8a2cfc5e369e68f661451ce8a4206
Aatmun Baxi <aatmun@pop-os.localdomain>
2023-10-09 14:48:56 -0500

    Revert "Finalize KNN and IRIS test"
    This reverts commit f10d57b466ef8870ad7c3ce02c9e589b412ec158.

--- src/lib.rs
+++ src/lib.rs
@@ -460,9 +460,4 @@ pub mod ml {
     }
     /// Submodule of `ml`: activation functions.
     pub mod activations;
-
-    /// Classification functions
-    pub mod classification {
-        pub mod k_nearest_neighbors;
-    }
 }
