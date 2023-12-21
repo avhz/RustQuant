6900203d9dad45e28ee49bba6c03b803bb2939ad
Daniel Boros <dancixx@gmail.com>
2023-12-22 00:41:27 +0100

    feat: improve fft fbm generator

--- src/stochastics/fractional_brownian_motion.rs
+++ src/stochastics/fractional_brownian_motion.rs
@@ -10,8 +10,9 @@
 use crate::stochastics::{StochasticProcess, Trajectories};
 use nalgebra::{DMatrix, DVector, Dim, Dyn, RowDVector};
 use ndarray::{concatenate, prelude::*};
-use ndrustfft::{ndfft, FftHandler};
-use num_complex::Complex;
+use ndarray_rand::RandomExt;
+use ndrustfft::{ndfft_par, FftHandler};
+use num_complex::{Complex, ComplexDistribution};
 use rand::Rng;
 use rand_distr::StandardNormal;
 use rayon::prelude::*;
@@ -109,51 +110,42 @@ impl FractionalBrownianMotion {
         if !(0.0..=1.0).contains(&self.hurst) {
             panic!("Hurst parameter must be between 0 and 1");
         }
-
-        let r = Array::from_shape_fn((n + 1,), |i| {
-            if i == 0 {
+        let mut r = Array1::linspace(0.0, n as f64, n + 1);
+        r.par_mapv_inplace(|x| {
+            if x == 0.0 {
                 1.0
             } else {
-                0.5 * ((i as f64 + 1.0).powf(2.0 * self.hurst)
-                    - 2.0 * (i as f64).powf(2.0 * self.hurst)
-                    + (i as f64 - 1.0).powf(2.0 * self.hurst))
+                0.5 * ((x as f64 + 1.0).powf(2.0 * self.hurst)
+                    - 2.0 * (x as f64).powf(2.0 * self.hurst)
+                    + (x as f64 - 1.0).powf(2.0 * self.hurst))
             }
         });
-
         let r = concatenate(
             Axis(0),
+            #[allow(clippy::reversed_empty_ranges)]
             &[r.view(), r.slice(s![..;-1]).slice(s![1..-1]).view()],
         )
         .unwrap();
-
         let mut data = Array1::<Complex<f64>>::zeros(r.len());
         for (i, v) in r.iter().enumerate() {
             data[i] = Complex::new(*v, 0.0);
         }
         let mut r_fft = FftHandler::new(r.len());
-        let mut lambda = Array1::<Complex<f64>>::zeros(r.len());
-        ndfft(&data, &mut lambda, &mut r_fft, 0);
-        let lambda = lambda.mapv(|x| x.re / (2.0 * n as f64)).mapv(f64::sqrt);
-
-        let mut rng = rand::thread_rng();
-        let mut rnd = Array1::<Complex<f64>>::zeros(2 * n);
-        for (_, v) in rnd.iter_mut().enumerate() {
-            let real: f64 = rng.sample(StandardNormal);
-            let imag: f64 = rng.sample(StandardNormal);
-            *v = Complex::new(real, imag);
-        }
-
-        let mut fgn = Array1::<Complex<f64>>::zeros(2 * n);
-        for (i, v) in rnd.iter().enumerate() {
-            fgn[i] = lambda[i] * v;
-        }
-        let mut fgn_fft_handler = FftHandler::new(2 * n);
+        let mut sqrt_eigenvalues = Array1::<Complex<f64>>::zeros(r.len());
+        ndfft_par(&data, &mut sqrt_eigenvalues, &mut r_fft, 0);
+        sqrt_eigenvalues.par_mapv_inplace(|x| Complex::new((x.re / (2.0 * n as f64)).sqrt(), x.im));
+        let rnd = Array1::<Complex<f64>>::random(
+            2 * n,
+            ComplexDistribution::new(StandardNormal, StandardNormal),
+        );
+        let fgn = &sqrt_eigenvalues * &rnd;
+        let mut fft_handler = FftHandler::new(2 * n);
         let mut fgn_fft = Array1::<Complex<f64>>::zeros(2 * n);
-        ndfft(&fgn, &mut fgn_fft, &mut fgn_fft_handler, 0);
-
-        let fgn = fgn_fft.slice(s![1..n + 1]).mapv(|x| x.re);
-        fgn.mapv(|x| (x * (n as f64).powf(-self.hurst)) * t_n.powf(self.hurst))
-            .to_vec()
+        ndfft_par(&fgn, &mut fgn_fft, &mut fft_handler, 0);
+        let fgn = fgn_fft
+            .slice(s![1..n + 1])
+            .mapv(|x: Complex<f64>| (x.re * (n as f64).powf(-self.hurst)) * t_n.powf(self.hurst));
+        fgn.to_vec()
     }
 }
 
