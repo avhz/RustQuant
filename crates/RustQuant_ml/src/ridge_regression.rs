96064194586cb5b303a1ba84eb429e44ce999f71
Yasser Naji <yfnaji@gmail.com>
2025-10-12 14:02:39 +0100

    Ridge: Implementations for constructor, fitting and create output struct

--- crates/RustQuant_ml/src/ridge_regression.rs
+++ crates/RustQuant_ml/src/ridge_regression.rs
@@ -48,3 +48,69 @@ pub struct RidgeRegressionOutput<T> {
     /// The coefficients of the ridge regression,
     pub coefficients: DVector<T>,
 }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// IMPLEMENTATIONS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+impl RidgeRegressionInput<f64> {
+    /// Create a new `RidgeRegressionInput` struct.
+    #[must_use]
+    pub fn new(
+        x: DMatrix<f64>,
+        y: DVector<f64>,
+        lambda: f64,
+        fit_intercept: bool,
+        max_iter: usize,
+        tolerance: f64,
+    ) -> Self {
+        Self { x, y, lambda, fit_intercept, max_iter, tolerance }
+    }
+
+    /// Fits a ridge regression to the input data.
+    /// Returns the intercept and coefficients.
+    /// The intercept is the first value of the coefficients.
+    pub fn fit(&self) -> Result<RidgeRegressionOutput<f64>, RustQuantError> {
+
+        let features_matrix = if self.fit_intercept {
+            self.x.clone().insert_column(0, 1.)
+        } else {
+            self.x.clone()
+        };
+
+        let n_col: usize = features_matrix.ncols();
+        let features_matrix_transpose = features_matrix.transpose();
+        let mut regularization_matrix = DMatrix::<f64>::identity(n_col, n_col);
+        
+        if self.fit_intercept { regularization_matrix[(0,0)] = 0.0; }
+    
+        let ridge_matrix = (&features_matrix_transpose * features_matrix) + self.lambda * regularization_matrix;
+
+        let ridge_matrix_inv = ridge_matrix
+            .try_inverse()
+            .ok_or(RustQuantError::MatrixInversionFailed)?;
+
+        let mut coefficients = ridge_matrix_inv * &features_matrix_transpose * &self.y;
+        let intercept: f64 =  if self.fit_intercept {
+            coefficients[0]
+        } else {
+            coefficients = coefficients.insert_row(0, 0.0); 
+            0.0
+        };
+
+        Ok(RidgeRegressionOutput {
+            intercept,
+            coefficients,
+        })
+    }
+}
+
+impl RidgeRegressionOutput<f64> {
+    /// Predicts the output for the given input data.
+    pub fn predict(&self, input: DMatrix<f64>) -> Result<DVector<f64>, RustQuantError> {
+        let intercept = DVector::from_element(input.nrows(), self.intercept);
+        let coefficients = self.coefficients.clone().remove_row(0);
+        let predictions = input * coefficients + intercept;
+        Ok(predictions)
+    }
+}
