d9d2583281b7a0a2df553c7dd4027fce28dc0644
Yasser Naji <yfnaji@gmail.com>
2025-10-12 14:03:49 +0100

    Ridge: Create unit tests

--- crates/RustQuant_ml/src/ridge_regression.rs
+++ crates/RustQuant_ml/src/ridge_regression.rs
@@ -114,3 +114,144 @@ impl RidgeRegressionOutput<f64> {
         Ok(predictions)
     }
 }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// UNIT TESTS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#[cfg(test)]
+mod tests_ridge_regression {
+    use super::*;
+    use std::f64::EPSILON as EPS;
+    use RustQuant_utils::assert_approx_equal;
+
+    struct DataForTests {
+        training_set: DMatrix<f64>,
+        testing_set: DMatrix<f64>,
+        response: DVector<f64>,
+    }
+
+    fn setup_test() -> DataForTests {
+        DataForTests {
+            training_set: DMatrix::from_row_slice(
+            4,
+            3,
+        &[
+                -0.083_784_355, -0.633_485_70, -0.399_266_60,
+                -0.982_943_745,  1.090_797_46, -0.468_123_05,
+                -1.875_067_321, -0.913_727_27,  0.326_962_08,
+                -0.186_144_661,  1.001_639_71, -0.412_746_90],
+            ),
+
+            testing_set: DMatrix::from_row_slice(
+            4,
+            3,
+            &[
+                0.562_036_47, 0.595_846_45, -0.411_653_01,
+                0.663_358_26, 0.452_091_83, -0.294_327_15,
+                -0.602_897_28, 0.896_743_96, 1.218_573_96,
+                0.698_377_69, 0.572_216_51, 0.244_111_43],
+            ),
+
+            response: DVector::from_row_slice(
+                &[
+                    -0.445_151_96,
+                    -1.847_803_64,
+                    -0.628_825_31,
+                    -0.861_080_69
+                ]
+            ),
+        }
+    }
+
+    #[test]
+    fn test_ridge_regression_without_intercept() -> Result<(), RustQuantError> {
+
+        let data: DataForTests = setup_test();
+
+        let input: RidgeRegressionInput<f64> = RidgeRegressionInput {
+            x: data.training_set,
+            y: data.response,
+            lambda: 1.0,
+            fit_intercept: false,
+            max_iter: 1000,
+            tolerance: 1e-4,
+        };
+
+        let output = input.fit()?;
+
+        for (i, coefficient) in output.coefficients.iter().enumerate() {
+            assert_approx_equal!(
+                coefficient,
+                &[
+                    0.0,
+                    0.620_453_495_948_496_1,
+                    -0.420_204_780_485_896_43,
+                    0.490_065_457_911_238_96
+                ][i],
+                EPS
+            );
+        }
+
+        let predictions = output.predict(data.testing_set)?;
+        for (i, pred) in predictions.iter().enumerate() {
+            assert_approx_equal!(
+                pred,
+                &[
+                    -0.103_396_954_909_688_48,
+                    0.077_372_233_758_234_32,
+                    -0.153_704_818_231_581,
+                    0.312_493_346_002_296_7
+                ][i],
+                EPS
+            );
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_ridge_regression_with_intercept() -> Result<(), RustQuantError> {
+
+        let data: DataForTests = setup_test();
+
+        let input: RidgeRegressionInput<f64> = RidgeRegressionInput {
+            x: data.training_set,
+            y: data.response,
+            lambda: 1.0,
+            fit_intercept: true,
+            max_iter: 1000,
+            tolerance: 1e-4,
+        };
+
+        let output = input.fit()?;
+
+        for (i, coefficient) in output.coefficients.iter().enumerate() {
+            assert_approx_equal!(
+                coefficient,
+                &[
+                    -0.701_404_539_262_792_8,
+                    0.215_855_099_335_031_66,
+                    -0.371_997_155_606_467_07,
+                    0.104_115_015_026_450_71,
+                ][i],
+                EPS
+            );
+        }
+
+        let predictions = output.predict(data.testing_set)?;
+
+        for (i, pred) in predictions.iter().enumerate() {
+            assert_approx_equal!(
+                pred,
+                &[
+                    -0.844_598_545_101_076_9,
+                    -0.757_036_026_633_643_9,
+                    -1.038_257_347_797_051_1,
+                    -0.738_103_402_522_953_9,
+                ][i],
+                EPS
+            );
+        }
+        Ok(())
+    }
+}
