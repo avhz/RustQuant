a5974b4b2cea2c1f33aaf8e05adc2720b258b7d5
Yasser Naji <yfnaji@gmail.com>
2025-10-12 21:47:21 +0100

    Lasso: Create unit tests

--- crates/RustQuant_ml/src/lasso.rs
+++ crates/RustQuant_ml/src/lasso.rs
@@ -149,3 +149,143 @@ impl LassoOutput<f64> {
         Ok(predictions)
     }
 }
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// UNIT TESTS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#[cfg(test)]
+mod tests_lasso_regression {
+    use super::*;
+    use RustQuant_utils::assert_approx_equal;
+
+    struct DataForTests {
+        training_set: DMatrix<f64>,
+        testing_set: DMatrix<f64>,
+        response: DVector<f64>,
+    }
+
+    fn setup_test() -> DataForTests {
+        DataForTests {
+            training_set: DMatrix::from_row_slice(
+            4,
+            3,
+        &[
+                -0.083_784_355, -0.633_485_70, -0.399_266_60,
+                -0.982_943_745,  1.090_797_46, -0.468_123_05,
+                -1.875_067_321, -0.913_727_27,  0.326_962_08,
+                -0.186_144_661,  1.001_639_71, -0.412_746_90],
+            ),
+
+            testing_set: DMatrix::from_row_slice(
+            4,
+            3,
+            &[
+                0.562_036_47, 0.595_846_45, -0.411_653_01,
+                0.663_358_26, 0.452_091_83, -0.294_327_15,
+                -0.602_897_28, 0.896_743_96, 1.218_573_96,
+                0.698_377_69, 0.572_216_51, 0.244_111_43],
+            ),
+
+            response: DVector::from_row_slice(
+                &[
+                    -0.445_151_96,
+                    -1.847_803_64,
+                    -0.628_825_31,
+                    -0.861_080_69
+                ]
+            ),
+        }
+    }
+
+    #[test]
+    fn test_lasso_without_intercept() -> Result<(), RustQuantError> {
+
+        let data: DataForTests = setup_test();
+
+        let input: LassoInput<f64> = LassoInput {
+            x: data.training_set,
+            y: data.response,
+            lambda: 0.01,
+            fit_intercept: false,
+            max_iter: 1000,
+            tolerance: 1e-4,
+        };
+
+        let output: LassoOutput<f64> = input.fit()?;
+        let predictions = output.predict(data.testing_set)?;
+
+        for (i, coefficient) in output.coefficients.iter().enumerate() {
+            assert_approx_equal!(
+                coefficient,
+                &[
+                    0.0,
+                    0.743_965_706_491_596_7,
+                    -0.304_713_846_510_641_43,
+                    1.355_162_653_724_116_22,
+                ][i],
+                f64::EPSILON
+            );
+        }
+
+        for (i, pred) in predictions.iter().enumerate() {
+            assert_approx_equal!(
+                pred,
+                &[
+                    -0.321_283_589_676_737_6,
+                    -0.04310400559445471,
+                    0.9295807191488583,
+                    0.6760174510230131
+                ][i],
+                f64::EPSILON
+            );
+        }
+        Ok(())
+    }
+
+     #[test]
+    fn test_lasso_with_intercept() -> Result<(), RustQuantError> {
+
+        let data: DataForTests = setup_test();
+
+        let input: LassoInput<f64> = LassoInput {
+            x: data.training_set,
+            y: data.response,
+            lambda: 0.01,
+            fit_intercept: true,
+            max_iter: 1000,
+            tolerance: 1e-4,
+        };
+
+        let output: LassoOutput<f64> = input.fit()?;
+        let predictions = output.predict(data.testing_set)?;
+
+        for (i, coefficient) in output.coefficients.iter().enumerate() {
+            assert_approx_equal!(
+                coefficient,
+                &[
+                    0.009_633_706_736_496_328,
+                    0.750_479_303_541_854_1,
+                    -0.301_997_087_876_784_5,
+                    1.373_605_833_196_545_3,
+                ][i],
+                f64::EPSILON
+            );
+        }
+
+        for (i, pred) in predictions.iter().enumerate() {
+            assert_approx_equal!(
+                pred,
+                &[
+                    -0.313_962_423_203_417_3,
+                    -0.033_349_554_520_968_38,
+                    0.960_198_011_081_136_2,
+                    0.696_256_873_679_798_4,
+                ][i],
+                f64::EPSILON
+            );
+        }
+        Ok(())
+    }
+}
